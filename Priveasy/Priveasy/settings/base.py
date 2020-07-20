d668bf5a57406f8dfd38c8151f2600a0f4d66197
Noah Troy <Admin@Priveasy.org>
2020-07-20 15:19:08 -0400

    Modified settings to include a proper db backend

--- Priveasy/Priveasy/settings/base.py
+++ Priveasy/Priveasy/settings/base.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Priveasy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'priveasydb',
+        'USER': 'priveasyuser',
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
