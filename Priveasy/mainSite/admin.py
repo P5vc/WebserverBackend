9ad142eff525ae5bc10b439afa1b6bd910a80c11
Noah Troy <Admin@Priveasy.org>
2020-07-27 18:23:24 -0400

    Added admin registrations

--- Priveasy/mainSite/admin.py
+++ Priveasy/mainSite/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(AccountContents)
+class AccountContentsAdmin(admin.ModelAdmin):
+	list_display = ['user' , 'planType']
+
+@admin.register(Stats)
+class StatsAdmin(admin.ModelAdmin):
+	list_display = ['homeViews' , 'accountViews' , 'vpnDownloads']
+
+@admin.register(VPNProfile)
+class VPNProfileAdmin(admin.ModelAdmin):
+	list_display = ['vpnUsername' , 'server' , 'vpnProfileNum']
+
+@admin.register(Paste)
+class PasteAdmin(admin.ModelAdmin):
+	list_display = ['pasteID' , 'public']
+
+@admin.register(EmailForwarder)
+class EmailForwarder(admin.ModelAdmin):
+	list_display = ['forwarder' , 'forwardTo']
+
+@admin.register(ShortenedURL)
+class ShortenedURL(admin.ModelAdmin):
+	list_display = ['slug' , 'destinationURL' , 'clicks']
+
+@admin.register(ReferralCode)
+class ReferralCode(admin.ModelAdmin):
+	list_display = ['code']
+
+@admin.register(VPNServer)
+class VPNServer(admin.ModelAdmin):
+	list_display = ['serverID' , 'serverIP' , 'serverType']
+
+@admin.register(Discount)
+class Discount(admin.ModelAdmin):
+	list_display = ['code']
+
+@admin.register(BudgetCalculator)
+class BudgetCalculator(admin.ModelAdmin):
+	pass
+
+@admin.register(EmailUsers)
+class EmailUsers(admin.ModelAdmin):
+	pass
+
+@admin.register(ZcashPaymentAddresses)
+class ZcashPaymentAddresses(admin.ModelAdmin):
+	list_display = ['requested' , 'generated' , 'account']
