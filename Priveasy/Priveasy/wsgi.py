51d5f66137faaca85bf01e5228eaebc79b6eb0a3
Priveasy Admin <Admin@Priveasy.org>
2020-07-27 17:53:48 -0400

    Updated Priveasy app files

--- Priveasy/Priveasy/wsgi.py
+++ Priveasy/Priveasy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Priveasy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Priveasy.settings.pro')
 
 application = get_wsgi_application()
