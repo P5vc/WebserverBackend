d98ab3cb3de048986cfa18274cdb04653c41dded
Priveasy Admin <Admin@Priveasy.org>
2020-07-29 23:10:47 -0400

    Fixed some bugs

--- Priveasy/Priveasy/settings/base.py
+++ Priveasy/Priveasy/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 # Read the .env file:
 env = environ.Env()
